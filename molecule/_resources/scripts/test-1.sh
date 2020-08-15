c3d40370c12945d74f17a44a06a7579280a1c7cc
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-15 10:04:46 +0200

    updates!

--- molecule/_resources/scripts/test-1.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export TARGET=/tmp/test-1.result
-echo "Success!" > $TARGET
-cat $TARGET
--- molecule/_resources/scripts/vault-init.sh
+++ molecule/_resources/scripts/vault-init.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 # https://chairnerd.seatgeek.com/practical-vault-usage/
-#set -e
+set -e
 
 export VAULT_ADDR=http://127.0.0.1:8200
 export VAULT_SECRET=/tmp/vault-key.json
 
-if [ -n "$(command -v yum)" ]; then
-  yum -y install jq which
-fi
-
-if [ -n "$(command -v apt-get)" ]; then
-  apt-get -qy install jq
-fi
-
+# Check if vault is initialized;
+# if not, then initialize, unseal & store secrets to file
 if [ -f $VAULT_SECRET ]; then
-   echo "Vault already initialized!"
+  printf " INFO: Vault is already initialized! \n\n"
 else
-  `which vault` operator init -key-shares=1 -key-threshold=1 -format=json > $VAULT_SECRET
+  printf " INFO: Initializing Vault.. \n\n"
+  vault operator init -key-shares=1 -key-threshold=1 -format=json > $VAULT_SECRET
   chmod 0400 $VAULT_SECRET
-  jq -r '.unseal_keys_b64[0]' $VAULT_SECRET | xargs `which vault` operator unseal
+  jq -r '.unseal_keys_b64[0]' $VAULT_SECRET | xargs vault operator unseal
 fi
 
-jq -r '.root_token' $VAULT_SECRET | xargs `which vault` login
-`which vault` secrets list
+# Login to vault
+printf " INFO: Logging in to vault.. \n\n"
+jq -r '.root_token' $VAULT_SECRET | xargs vault login
 
-#`which vault` operator seal
+# List secrets
+printf " INFO: Listing secrets.. \n\n"
+vault secrets list
