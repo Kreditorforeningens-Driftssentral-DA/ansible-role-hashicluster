8b99e6fda7211d7f8bdd52a208cc5ac02029b897
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-18 10:31:29 +0200

    Check

--- .github/actions/molecule/docker-entrypoint.sh
+++ .github/actions/molecule/docker-entrypoint.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env sh
 set -e
 
-export ROLE_PATH=/tmp/$(basename ${GITHUB_REPOSITORY})
+# SET ENVIRONMENT
 export ANSIBLE_ROLES_PATH=/tmp
+export ROLE_PATH=/tmp/$(basename ${GITHUB_REPOSITORY})
 
+# LINK REPO TO ROLES PATH
 ln -fs `pwd` ${ROLE_PATH}
 cd ${ROLE_PATH}
 
+ansible --version
+molecule --version
+
 # VALIDATE INPUT
 if [ "$1" = 'molecule' ]; then
-  ansible --version
-  molecule --version
-  ansible-galaxy role list --roles-path .
+  #ansible-galaxy role list --roles-path .
   exec "$@"
 else
-  echo "ERROR: Only molecule-commands are accepted"
+  echo "ERROR: Only molecule-commands are accepted!"
 fi
 
 #echo "::set-output name=example::value-here"
