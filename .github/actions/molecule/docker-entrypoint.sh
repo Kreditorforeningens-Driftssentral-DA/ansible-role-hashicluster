837d14c16f90690286411631629c2ab15f50b873
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-18 08:40:51 +0200

    updates

--- .github/actions/molecule/docker-entrypoint.sh
+++ .github/actions/molecule/docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
-set -e
+#set -e
 
-env
-pwd
-ls -lts
-
-ln -s `pwd` "/tmp/${GITHUB_REPOSITORY}"
-cd "/tmp/${GITHUB_REPOSITORY}"
-ls -lts
-molecule --version \
+molecule --version
 ansible --version
+
+ln -fs `pwd` /tmp/${GITHUB_REPOSITORY}
+ls -lts cd /tmp/${GITHUB_REPOSITORY}
+
+cd /tmp/${GITHUB_REPOSITORY}
+
+#time=$(date)
+#echo "::set-output name=time::$time"
