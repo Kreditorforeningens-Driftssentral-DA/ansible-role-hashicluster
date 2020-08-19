d8705d8743abcfa814374e057e68def839cabf95
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-19 15:37:32 +0200

    test

--- .github/actions/molecule/docker-entrypoint.sh
+++ .github/actions/molecule/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
+cd ${GITHUB_REPOSITORY}
+
+# TODO: Validate command(s)
 if [ "$1" = 'molecule' ]; then
-  cd ${GITHUB_REPOSITORY}
   exec "$@"
 else
   echo "ERROR: Only molecule-commands are accepted!"
