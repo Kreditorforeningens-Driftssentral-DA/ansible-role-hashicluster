4f84d3a78b7c46dcfc3f2302d56d1d0a087777cf
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-19 12:17:35 +0200

    Changed ubuntu images

--- .github/actions/molecule/docker-entrypoint.sh
+++ .github/actions/molecule/docker-entrypoint.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-env
-
-ansible --version
-molecule --version
-
-# SET ENVIRONMENT
-# ${GITHUB_REPOSITORY}
-#export ANSIBLE_ROLES_PATH=/tmp
-#export ROLE_PATH=/tmp/$(basename ${GITHUB_REPOSITORY})
-#ansible-galaxy role list --roles-path .
-
-# LINK REPO TO ROLES PATH
-#ln -fs `pwd` ${ROLE_PATH}
-#ls -lts /tmp
-
 # VALIDATE INPUT
 if [ "$1" = 'molecule' ]; then
   cd ${GITHUB_REPOSITORY}
