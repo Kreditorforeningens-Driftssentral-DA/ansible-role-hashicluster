92275b7e900295766ec70c3b503de9c744097721
Rune Rønneseth <rune.ronneseth@kred.no>
2020-08-19 13:56:28 +0200

    Testing with premade image

--- .github/actions/molecule/docker-entrypoint.sh
+++ .github/actions/molecule/docker-entrypoint.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-# VALIDATE INPUT
 if [ "$1" = 'molecule' ]; then
   cd ${GITHUB_REPOSITORY}
   exec "$@"
 else
   echo "ERROR: Only molecule-commands are accepted!"
 fi
-
-#echo "::set-output name=example::value-here"
